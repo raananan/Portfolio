12ece8e0734927d033f2ace1283d097ab92371a7
raananan <raananhein20692@gmail.com>
2020-12-15 19:59:15 +0200

    Add files via upload

---
+++ Threads/MyProcMng/MyProcMng/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MyProcMng
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<MyProcess> processList = new List<MyProcess>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            ReadProcess();
+
+        }
+
+        private void ReadProcess()
+        {
+            int multiThreadCount = 0;
+            Process[] process_arr = Process.GetProcesses();
+            MyProcess my_process;
+            foreach (var p in process_arr)
+            {
+                my_process = new MyProcess();
+                my_process.PId = p.Id;
+                my_process.ProcessName = p.ProcessName;
+                my_process.ThreadsCount = p.Threads.Count;
+                processList.Add(my_process);
+                multiThreadCount = my_process.ThreadsCount > 1 ? multiThreadCount +1 : multiThreadCount;
+
+            }
+
+            this.lsProc.ItemsSource = processList;
+            MessageBox.Show(string.Format("multi theards process count is: {0}%", 
+                ((double)multiThreadCount / processList.Count) * 100));
+        }
+
+    }
+}
---
+++ Threads/MyProcMng/MyProcMng/MyProcess.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace MyProcMng
+{
+    public class MyProcess : INotifyPropertyChanged
+    {
+        private int pId;
+        private string processName;
+        public int threadsCount;
+
+
+        public int PId
+        {
+            get { return this.pId; }
+            set{this.pId = value;}
+
+
+        }
+
+        public string ProcessName
+        {
+            get { return this.processName; }
+            set
+            { this.processName = value;}
+
+        }
+
+        public int ThreadsCount
+        {
+            get { return this.threadsCount; }
+            set
+            {this.threadsCount = value;}
+
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
